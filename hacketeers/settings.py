5b633a73cb4a48029a5373d326967872231f32a0
Leonid Tkachenko <18leonidtkachenko@gmail.com>
2022-09-12 07:01:09 -0400

    allow cors

--- hacketeers/settings.py
+++ hacketeers/settings.py
@@ -173,8 +173,7 @@ SIMPLE_JWT = {
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-CORS_ALLOW_HEADERS = list(default_headers) + ['cache-control', 'access-control-allow-headers', 'access-control-allow-methods',
-                                              'access-control-request-method', 'access-control-request-headers']
+CORS_ALLOW_HEADERS = list(default_headers)
 CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
-CORS_ALLOW_ALL_ORIGINS = bool(os.environ.get('CORS_ALLOW_ALL_ORIGINS', 0))
-CORS_ALLOW_CREDENTIALS = bool(os.environ.get('CORS_ALLOW_CREDENTIALS', 0))
+CORS_ALLOW_ALL_ORIGINS = bool(os.environ.get('CORS_ALLOW_ALL_ORIGINS', 1))
+CORS_ALLOW_CREDENTIALS = bool(os.environ.get('CORS_ALLOW_CREDENTIALS', 1))
